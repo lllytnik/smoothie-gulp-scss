09faeff65003c2d73577fa4222e7f338452d864f
lllytnik <65924940+lllytnik@users.noreply.github.com>
2021-05-21 00:31:11 +0100

    Updates

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+(function() {
+    const burger = document.querySelector('.header__burger-btn');
+    const navigation = document.querySelector('.header__navigation');
+    
+    burger.addEventListener('click', () => {
+        navigation.classList.toggle('active');
+    });
+}());
+//# sourceMappingURL=script.js.map
